77d6ba1c447e7885784fd1f75b58845684b8b44b
Mahdi sayyadi <96723318+Sayyadi68@users.noreply.github.com>
2026-09-06 13:43:24 +0330

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'تنخواه گردون',
+    'version': '17.0.1.0.0',
+    'category': 'تنخواه/تنخواه',
+    'summary': 'سامانه مدیریت تنخواه کارکنان',
+    'description': 'سامانه مدیریت تنخواه کارکنان',
+    'author': 'صیادی-فهیم-پورشهامی',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+    'installable': True,
+}
